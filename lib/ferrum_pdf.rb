bc34c3a9e3c58a2073ab0737f6342e44b5bb1dcb
Matouš Borák <matous.borak@platanus.cz>
2024-10-24 21:14:51 +0200

    Fix browser restaring

--- lib/ferrum_pdf.rb
+++ lib/ferrum_pdf.rb
@@ -17,7 +17,7 @@ module FerrumPdf
 
   class << self
     def browser(**options)
-      @browser_options = options
+      @browser_options ||= options
       @browser ||= Ferrum::Browser.new(options)
     end
 
@@ -43,12 +43,11 @@ module FerrumPdf
           page.go_to(url)
         end
         yield page
-      rescue Ferrum::DeadBrowserError
-        sleep 0.1
-        @browser.restart
-        @browser = Ferrum::Browser.new(@browser_options)
-        retry
       end
+    rescue Ferrum::DeadBrowserError
+      browser.restart
+      @browser = Ferrum::Browser.new(@browser_options)
+      retry
     end
   end
 end
